c699a3eb2b135d7fad228648e7e878966e588215
Sean Nguyen <seannguyen2919@gmail.com>
2020-10-13 15:07:55 -0400

    inital creation

---
+++ AUX/AUX/AUXApp.swift
@@ -0,0 +1,17 @@
+//
+//  AUXApp.swift
+//  AUX
+//
+//  Created by Sean Nguyen on 2020-10-13.
+//
+
+import SwiftUI
+
+@main
+struct AUXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AUX/AUX/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AUX
+//
+//  Created by Sean Nguyen on 2020-10-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
