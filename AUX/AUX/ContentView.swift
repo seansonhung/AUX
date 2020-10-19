0071243dd6b46be926a67f7a7ca3b899808c19a3
Sean Nguyen <seannguyen2919@gmail.com>
2020-10-19 17:08:12 -0400

    add button

--- AUX/AUX/ContentView.swift
+++ AUX/AUX/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button(action: {
+            print("Pressed!")
+            }) {
+            Image("cow")
+                .clipShape(Circle())
+                .overlay(
+                    Circle().stroke(Color.gray, lineWidth: 4))
+                .shadow(radius: 10)
+        }
     }
 }
 
