a4dac46cab71bf35d9de9f917361aa2cec3dbe49
Sean Nguyen <seannguyen2919@gmail.com>
2020-10-23 18:41:53 -0400

    bluetooth initial

---
+++ AUX/AUX/ParticlePeripheral.swift
@@ -0,0 +1,20 @@
+//
+//  ParticlePeripheral.swift
+//  AUX
+//
+//  Created by Sean Nguyen on 2020-10-23.
+//
+
+import UIKit
+import CoreBluetooth
+
+class ParticlePeripheral: NSObject {
+
+    /// MARK: - Particle LED services and charcteristics Identifiers
+
+    public static let particleLEDServiceUUID     = CBUUID.init(string: "b4250400-fb4b-4746-b2b0-93f0e61122c6")
+    public static let redLEDCharacteristicUUID   = CBUUID.init(string: "b4250401-fb4b-4746-b2b0-93f0e61122c6")
+    public static let greenLEDCharacteristicUUID = CBUUID.init(string: "b4250402-fb4b-4746-b2b0-93f0e61122c6")
+    public static let blueLEDCharacteristicUUID  = CBUUID.init(string: "b4250403-fb4b-4746-b2b0-93f0e61122c6")
+
+}
